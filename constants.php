8e0a2e6f813fb0e6c6b2f6fe0e43241328680e05
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-30 16:16:45 -0400

    just a constant

---
+++ constants.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  
+<?php
+    
+define("NAME", 1000);
+        
+?>
+   
+   <h1><?php echo NAME?></h1>
+    
+</body>
+</html>
