e3138aea318876db5cb374575dea615f1b0addc2
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-30 16:37:24 -0400

    some math functions

---
+++ maths.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  
+<?php
+    //php.net/manual/en/ref.math.php
+    // 3^3
+    echo pow(3, 3);
+    echo "<br>";
+    // return a random, param is two numbers inclusive
+    echo rand(1, 10);
+    echo "<br>";
+    // square root
+    echo sqrt(100);
+    echo "<br>";
+    //rounds up
+    echo ceil(2.9);
+    echo "<br>";
+    //rounds down
+    echo floor(2.4);
+    echo "<br>";
+    
+?>
+  
+   
+    
+</body>
+</html>
