86d2c1a9f682b05341ba96003a2091aa10b8f0b3
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-30 14:24:08 -0400

    the classic for loop

---
+++ forLoop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+for($i=-5; $i <= 5; $i++){
+    echo $i;
+    echo "<br>";
+}
+    
+$title = "this is a dynamic title"    
+    
+?>
+   
+   <h1><?php echo $title ?></h1>
+    
+</body>
+</html>
