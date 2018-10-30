433f06ae1f891ed3552385fb762cada40c108f4f
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-30 14:16:25 -0400

    learning the while loop

---
+++ while.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  
+<?php
+$num1 = -5;
+
+    
+while($num1 <=5){
+    
+    echo $num1;
+    $num1++;
+    
+    echo "<br>";
+    
+    
+}    
+    
+$title = "this is a dynamic title"    
+    
+?>
+   
+   <h1><?php echo $title ?></h1>
+    
+</body>
+</html>
