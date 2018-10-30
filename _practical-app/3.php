4e65e0c09e93f7b2b498edc09bc2b8af6067f119
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-30 14:58:34 -0400

    suck it mini quiz

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -15,15 +15,49 @@
 <?php  
 
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
-
-
-
 	Step 2: Make a forloop  that displays 10 numbers
-
-
 	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
  */
+    
+$num1 = 0;
+    
+if($num1 == 1){
+    echo"i wont print";
+    echo "<br>";
+} elseif($num1 == -1){
+    echo"i wont print either";
+    echo "<br>";
+}else{
+    echo"i print";
+    echo "<br>";
+};
+    
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 0];
+
+foreach($numbers as $num){
+    echo $num;
+    echo "<br>";
+};
+
+switch($num1){
+    case 1:
+        echo "it is 1";
+        break;
+    case 2:
+        echo "it is 2";
+        break;
+    case 3:
+        echo "it is 3";
+        break;
+    case 4:
+        echo "it is 4";
+        break;
+    case 5:
+        echo "it is 5";
+        break;
+    default:
+        echo "idk but this will probably print";
+}
 
 	
 ?>
