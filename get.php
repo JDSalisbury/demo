d9113aa385e2a59cba698616d80bf2542dff2eaf
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-31 16:29:29 -0400

    learning about the get

---
+++ get.php
@@ -0,0 +1,27 @@
+
+<?php
+    
+$_GET;
+
+print_r($_GET);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  <a href="get.php?id=200">click here</a>
+<?php
+    
+
+    
+?>
+   
+ 
+    
+</body>
+</html>
