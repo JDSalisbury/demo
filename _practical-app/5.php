f519e1e19792f2cada227aed0f64cd790a2204ba
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-31 09:34:29 -0400

    boring

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -16,15 +16,23 @@
 
 
 /*  Step1: Use a pre-built math function here and echo it
-
-
 	Step 2:  Use a pre-built string function here and echo it
-
-
 	Step 3:  Use a pre-built Array function here and echo it
-
  */
 
+    //maths
+    echo pi();
+    echo "<br>";
+    //string stuff
+    $str = "something something darkside";
+    echo strrev($str);
+    echo "<br>";
+    //array thing
+    $ary = [1, "number", "hat", "bear", "fish", "turtle", "lemon"];
+    $randKey =  array_rand($ary);
+    echo $ary[$randKey];
+
+    
 	
 ?>
 
