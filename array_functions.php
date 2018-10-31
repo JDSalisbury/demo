c4c5bf0b1a402c66ca0f776388b28eec54168adf
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-31 09:16:47 -0400

    just a few array stuff, more at php.net/manual/en/ref.array.php

---
+++ array_functions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  
+<?php
+    //php.net/manual/en/ref.array.php
+    
+    $list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    
+        echo max($list);
+        echo "<br>";
+    
+        echo min($list);
+        echo "<br>";
+    
+        print_r($list);
+        echo "<br>";
+
+?>
+   
+ 
+    
+</body>
+</html>
