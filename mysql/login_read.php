2751f3054d38bf9b4093c271107bf5797ae3eb48
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-31 11:52:13 -0400

    print_r($row);

---
+++ mysql/login_read.php
@@ -0,0 +1,58 @@
+<?php
+
+
+    
+$connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+    echo "<br>";
+    if($connection){
+        echo "we are connected";
+    } else {
+        die("database connection error");
+    }
+    
+    $query = "SELECT * FROM users";
+    
+    $result =  mysqli_query($connection, $query);
+
+    if(!$result){
+        die("query failed" . mysqli_error());
+    }
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+   <div class="container">
+       <div class="col-sm-6">
+          
+        <?php
+  
+           while($row = mysqli_fetch_assoc($result)){
+               ?>
+               <pre>
+               <?php
+                print_r($row);
+                ?>
+               </pre>
+            <?php
+           }
+           
+           
+        ?>
+        
+       </div>
+   </div>
+    
+</body>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</html>
