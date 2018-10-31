ed8c3353896b449791aa831ca812ebeff1c7072f
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-31 16:47:00 -0400

    quick post stuff

---
+++ posting.php
@@ -0,0 +1,31 @@
+<?php
+echo $_POST['name'];
+
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  <form action="posting.php" method="POST">
+      
+      <input type="text" name="name">
+      <input type="submit">
+      
+      
+  </form>
+<?php
+    
+
+    
+?>
+   
+ 
+    
+</body>
+</html>
