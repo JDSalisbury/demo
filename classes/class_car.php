92768a1643b7c9f8d51f9d87b0a6da3c9c45c1f7
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-01 11:41:45 -0400

    things can be static too with different strange rules that follow with it

--- classes/class_car.php
+++ classes/class_car.php
@@ -6,10 +6,15 @@ class Car {
     public $wheels = 4;
     public $doors = 4;
     public $engine = 'v6';
+    static $radio = 'tape Deck';
     
     function moveWheels(){
         echo "Wheels are moving";
         echo "<br>";
+    }
+
+    function getRadio(){
+        return Car::$radio;
     }
 
     function getWheels(){
@@ -40,6 +45,12 @@ $bmw-> setDoors(2);
 echo $bmw->doors;
 echo "<br>";
 echo $volvo ->doors;
+echo "<br>";
 
+echo $bmw::$radio;
+echo "<br>";
+echo "<br>";
+
+echo $bmw -> getRadio();
 ?>
    
