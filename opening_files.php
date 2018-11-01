21a19e6531311e365f029514bcbd2253ccff1f7c
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-11-01 13:21:14 -0400

    reading said file

--- opening_files.php
+++ opening_files.php
@@ -4,7 +4,7 @@ $file = "example.txt";
 
 if($handle = fopen($file, 'w')){
 
-    fwrite($handle, "writing something else");
+    fwrite($handle, "asdflajsdf;lkjasdf;lkjasdf ;lkajsdf ;lksdf lkjlfsljfsls kdkjlfs");
 
 } else {
 
@@ -12,6 +12,16 @@ if($handle = fopen($file, 'w')){
 
 }
 
+
+if($handle = fopen($file, 'r')){
+
+  echo $content = fread($handle, filesize($file)); //each bite equals a char.
+
+} else {
+
+    echo "Something messed up, idk.";
+
+}
 
 fclose($handle);
 
